72f9743b72db109a4a531048b4ce9af196d4981f
kaushal <kaushalkishorroy21@gmail.com>
2024-08-24 22:13:13 +0530

    done

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+// Establish database connection
+connect();
+
+export async function POST(request: NextRequest) {
+  // extract data from token
+  const userId = await getDataFromToken(request);
+
+  const user = await User.findOne({ _id: userId }).select("-password");
+
+  // check if there is no user
+
+  return NextResponse.json({
+    message: "user found",
+    data: user,
+  });
+}
