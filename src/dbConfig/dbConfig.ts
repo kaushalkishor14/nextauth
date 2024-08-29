e1c5bbd962cfc277b5f797db6dd0802898af9492
kaushal <kaushalkishorroy21@gmail.com>
2024-08-29 21:55:32 +0530

    all done

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 export async function  connect (){
     try {
         
-        mongoose.connect(process.env.MONGO_URI!);
-        
+        // mongoose.connect(process.env.MONGO_URI!);
+        mongoose.connect(process.env.MONGO_URI!)
         const connection = mongoose.connection
 
         connection.on("connected", ()=>{
