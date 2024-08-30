666b3b86ba2cbef15d9e0762b43a00e5a7adf089
kaushal <kaushalkishorroy21@gmail.com>
2024-08-30 21:50:57 +0530

    style done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -46,34 +46,35 @@ function LoginPage() {
   return (
     <div className='flex justify-center flex-col items-center min-h-screen py-2
     '>
-    <h1 className='text-3xl font-bold font-mono p-2 text-orange-500'>{loading? "processing...":"login"}</h1>  
+    <div className='flex flex-col items-center w-1/2 border p-8 rounded-lg transparent bg-blue-300 '>
+    <h1 className='text-3xl font-bold  p-2 text-orange-500'>{loading? "processing...":"Login"}</h1>  
     <hr/>
-    <label htmlFor="email" className='text-lg  font-mono mb-2'>email</label>
+    <label htmlFor="email" className='text-gray-600 text-lg  mb-2'>Email</label>
     <input
-    className='border-2 text-black border-gray-300 p-2 rounded-lg focus:outline-none focus:border-gray-600'
+    className='border-2 text-black border-indigo-400 p-2 rounded-lg focus:outline-none focus:border-gray-600 bg-gray-200'
     id='email'
     value={user.email}
     onChange={(e)=>setUser({...user,email:e.target.value})}
     />
 
-    <label htmlFor="password" className='text-lg font-mono mb-2 mt-4'>password</label>
+    <label htmlFor="password" className='text-lg text-gray-600  mb-2 mt-4'>Password</label>
     <input
-    className='border-2 text-black border-gray-300 p-2 rounded-lg focus:outline-none focus:border-gray-600 mb-4'
+    className='border-2 border-indigo-400 text-black p-2 rounded-lg focus:outline-none focus:border-gray-600 mb-4 bg-gray-200'
     id='password'
     value={user.password}
     onChange={(e)=>setUser({...user,password:e.target.value})}
     
     />
 
-    <button className='border-2  border-gray-300 p-2 rounded-lg focus:outline-none focus:border-gray-600 my-2 bg-orange-500 text-white font-mono' 
+    <button className='border-2  border-gray-300 p-2 rounded-lg focus:outline-none focus:border-gray-600 my-2 bg-orange-500 text-white font-mono hover:bg-slate-500 hover:scale-110' 
     onClick={onLogin}
     >
        {buttonDisable? "processing..." : "login"}
 
     </button>
 
-    <Link href={"/signup"}>Visit to Signup page</Link>
-
+    <Link className='text-lg font-mono text-red-400' href={"/signup"}>Visit to Signup page</Link>
+    </div>
 
       </div>
   )
